9bf1b7fa36e9593100d17d60f447b95daf9e14f6
vouchh <vouchhean.mom@institute.pse.ngo>
2025-02-07 09:10:57 +0700

    last

--- pkmis/services/dashboard/amount.prmium.ts
+++ pkmis/services/dashboard/amount.prmium.ts
@@ -3,9 +3,7 @@ import prisma from "@/lib/prisma";
 export async function getTotalamountPremium() {
   const data = await prisma.$queryRaw<{ amount: string | null }[]>`
   select sum(proposed_premium) AS amount from quotation ;
-
   `;
-
   if (data.length > 0) {
     return data[0].amount ? data[0].amount : "0";
   } else {
